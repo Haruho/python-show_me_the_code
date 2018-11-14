db40b0246465e4ee7f93984ee38dc35bf976e156
Haruho <532685083@qq.com>
2018-11-14 09:18:50 +0800

    filter words in sentence

--- 011/filter.py
+++ 011/filter.py
@@ -5,14 +5,21 @@ invaild = []
 path = 'E:/PythonProject/Python3/011/filtered_words.txt'
 
 temp = open(path,encoding = 'UTF-8').readlines()
-
+endresult = ''
 #排除列表中‘/n’的影响
 for i in temp:
     invaild.append(i.strip('\n'))
 
-#print(invaild)
-inputstr = input('输入任何文字')
-if inputstr in invaild:
-    print('Human Rights')
-else:
-    print('Freedom') 
+inputstr = str(input('输入任何文字'))
+
+
+def combainresult(sentence):
+    for i in invaild:
+        if i in sentence:
+            #回调
+            combainresult(sentence.replace(i,'**'))
+            #结果赋值
+            sentence = sentence.replace(i,'**')
+    return sentence
+
+print(combainresult(inputstr))
